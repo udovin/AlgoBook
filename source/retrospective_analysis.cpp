c70dd569fc32b2f47d58a85c85e4f68bcce0e6a9
ShavelV <valshavel@gmail.com>
2018-11-24 20:49:35 +0300

    Retrospective analysis added

---
+++ source/retrospective_analysis.cpp
@@ -0,0 +1,58 @@
+//PLEASE BE CAREFUL WITH ML, SOMETIMES YOU NEED TO BUILD A GRAPH ON A MOVE
+class Analysis
+{
+private:
+
+    int n;
+    vector< vector<int> > g;
+    vector<char> state;
+    vector<char> used;
+    vector<int> degree;
+
+    void dfs (int v)
+    {
+        used[v] = true;
+        for(auto x : g[v])
+        {
+            if(!used[x])
+            {
+                if(state[v] == -1)
+                    state[x] = 1;
+                else if(--degree[x] == 0)
+                    state[x] = -1;
+                else
+                    continue;
+                dfs(x);
+            }
+        }
+    }
+public:
+    Analysis(int n)
+        : n(n), g(n), state(n), used(n), degree(n) {}
+
+    void addEdge(int u, int v)
+    {
+        g[v].push_back(u);
+        degree[u]++;
+    }
+
+    void addState(int x, bool f)
+    {
+        if(f)
+            state[x] = 1;
+        else
+            state[x] = -1;
+    }
+
+    vector<char> solve()
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(!used[i] && state[i])
+            {
+                dfs(i);
+            }
+        }
+        return state;
+    }
+};
