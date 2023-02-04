04d2f60b7b4bda73e4d11c2121a4fe1726b605f3
Ivan Udovin <42046217+udovin@users.noreply.github.com>
2023-02-04 03:41:08 +0300

    Replace vector with array

--- source/hash_string.cpp
+++ source/hash_string.cpp
@@ -1,13 +1,13 @@
-class HashString {
+template<int N = 2> class HashString {
 private:
-	vector<int> a, m;
-	vector<vector<int>> h, p;
+	array<int, N> a, m;
+	array<vector<int>, N> h, p;
 
 public:
 	HashString(
-		const string& s, const vector<int>& a, const vector<int>& m)
-		: a(a), m(m), h(a.size()), p(a.size()) {
-		for (int i = 0; i < m.size(); i++) {
+		const string& s, const array<int, N>& a = {31, 37}, const array<int, N>& m = {int(1e9) + 7, int(1e9) + 9})
+		: a(a), m(m), h{}, p{} {
+		for (int i = 0; i < N; i++) {
 			h[i].resize(s.size() + 1);
 			p[i].resize(s.size() + 1);
 			h[i][0] = 0, p[i][0] = 1;
@@ -20,11 +20,11 @@ public:
 	}
 
 	bool check(int a, int b, int l) const {
-		for (int i = 0; i < m.size(); i++) {
+		for (int i = 0; i < N; i++) {
 			int x = h[i][a + l] - h[i][b + l];
 			if (x < 0)
 				x += m[i];
-			int y = (h[i][a] - h[i][b]) * (long long)p[i][l] % m[i];
+			int y = (h[i][a] - h[i][b]) * i64(p[i][l]) % m[i];
 			if (y < 0)
 				y += m[i];
 			if (x != y)
@@ -33,22 +33,22 @@ public:
 		return true;
 	}
 
-	vector<int> get(int l, int r) const {
-		vector<int> x(m.size());
-		for (int i = 0; i < m.size(); i++) {
-			x[i] = (h[i][r] - h[i][l] * (long long)p[i][r - l]) % m[i];
+	array<int, N> get(int l, int r) const { // [l, r)
+		array<int, N> x{};
+		for (int i = 0; i < N; i++) {
+			x[i] = (h[i][r] - h[i][l] * i64(p[i][r - l])) % m[i];
 			if (x[i] < 0)
 				x[i] += m[i];
 		}
 		return x;
 	}
 
-	vector<int> get(const string& s) const {
-		vector<int> x(m.size());
+	array<int, N> get(const string& s) const {
+		array<int, N> x{};
 		// Or replace s[j] with (s[j] - FIRST_CHAR)
-		for (int i = 0; i < m.size(); i++)
+		for (int i = 0; i < N; i++)
 			for (int j = 0; j < s.size(); j++)
-				x[i] = (x[i] * (long long)a[i] + s[j]) % m[i];
+				x[i] = (x[i] * i64(a[i]) + s[j]) % m[i];
 		return x;
 	}
 };
